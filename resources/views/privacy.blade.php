4394d030a66cb58ecd849087dd0e8f70122c2c14
Itotagawa <itotagawa@mj-star.jp>
2020-03-30 20:16:20 +0900

    webページ追加

---
+++ resources/views/privacy.blade.php
@@ -0,0 +1,180 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<meta name="description" content="公益社団法人 兵庫県保育協会の個人情報保護方針です。" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+<meta name="format-detection" content="telephone=no" />
+<link rel="icon" href="/img/favicon.ico" />
+<link rel="shortcut icon" href="/img/favicon.ico" type="image/vnd.microsoft.icon" />
+<link rel="icon" href="/img/favicon.ico" type="image/vnd.microsoft.icon" />
+<link rel="shortcut icon" href="/img/favicon.ico" type="image/x-icon" />
+<meta name="robots" content="noindex,nofollow"/>
+<title>公益社団法人 兵庫県保育協会 | 個人情報保護方針</title>
+<script type="text/javascript" src="//webfont.fontplus.jp/accessor/script/fontplus.js?kZSFUtx-OUM%3D&box=93sB9wS4lok%3D&aa=1&ab=2" charset="utf-8"></script>
+
+<link href="{{ asset('css/web/swiper.css') }}" rel="stylesheet">
+<link href="{{ asset('css/web/style.css') }}" rel="stylesheet">
+
+</head>
+<body id="home">
+<!--[if lte IE 10]>
+<div class="browserhappy">
+Internet Explorerの安全ではないバージョンをお使いのようです。古いWebブラウザを使い続けるとパソコンに危険性が生じます。今すぐブラウザをアップグレードしましょう。
+<a href="http://browsehappy.com/" target="_blank">今すぐアップグレード！</a>
+</div>
+<![endif]-->
+<div class="wbx"></div>
+<div class="wrp">
+  <!-- btn-to-top -->
+  <div id="btnToTopBox" class="btn-to-top-float-box">
+    <div class="btn-to-top-box">
+      <a href="javascript:;" id="btnToTop" class="btn-to-top"></a>
+    </div>
+  </div>
+
+  <!-- header -->
+  <header id="header" class="header">
+    <div id="headerInner" class="header-inner">
+      <div class="header-box">
+        <div class="header-logo">
+          <a href="/" class="header-logo-a">
+            <div class="header-logo-txt"><span class="logo-txt-s">公益社団法人 兵庫県保育協会</span><br /><span class="logo-txt-l">研修サイト</span></div>
+          </a>
+        </div>
+        <div class="header-nav">
+          <div class="header-nav-1">
+            <a href="{{ route('links') }}" class="header-nav-link">リンク</a>
+            <a href="{{ route('privacy') }}" class="header-nav-link">個人情報保護方針</a>
+          </div>
+          <div class="header-nav-2">
+            <a href="{{ route('login') }}" class="header-nav-btn header-nav-btn-shisetsu">施設ログイン</a>
+            <a href="{{ route('login') }}" class="header-nav-btn header-nav-btn-jimukyoku">事務局ログイン</a>
+          </div>
+        </div>
+        <div class="header-icon">
+          <div id="navIcon" class="nav-toggle nav-toggle-open">
+            <span class="nav-toggle-bar"></span>
+            <span class="nav-toggle-bar"></span>
+            <span class="nav-toggle-bar"></span>
+            <span class="nav-toggle-text"></span>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- nav -->
+    <nav id="navMenu" class="nav">
+      <div class="nav-inner">
+        <ul class="nav-ul-1">
+          <li class="nav-ul-1-li">
+            <a href="{{ route('greeting') }}" class="nav-ul-1-li-a"><span class="nav-ul-1-li-a-span nav-ul-1-li-a-greeting">ごあいさつ</span></a>
+          </li>
+          <li class="nav-ul-1-li">
+            <a href="javascript:alert('現在準備中です');" class="nav-ul-1-li-a"><span class="nav-ul-1-li-a-span nav-ul-1-li-a-manual">操作マニュアル</span></a>
+          </li>
+          <li class="nav-ul-1-li">
+            <a href="{{ route('register') }}" class="nav-ul-1-li-a"><span class="nav-ul-1-li-a-span nav-ul-1-li-a-registration">ユーザー新規登録</span></a>
+          </li>
+          <li class="nav-ul-1-li">
+            <a href="{{ route('login') }}" class="nav-ul-1-li-a"><span class="nav-ul-1-li-a-span nav-ul-1-li-a-login">ユーザーログイン</span></a>
+          </li>
+          <li class="nav-ul-1-li">
+            <a href="{{ route('contact') }}" class="nav-ul-1-li-a"><span class="nav-ul-1-li-a-span nav-ul-1-li-a-contact">お問い合わせ</span></a>
+          </li>
+          <li class="nav-ul-1-li nav-ul-1-li-sp">
+            <a href="{{ route('login') }}" class="nav-ul-1-li-a"><span class="nav-ul-1-li-a-span nav-ul-1-li-a-shisetsu">施設ログイン</span></a>
+          </li>
+          <li class="nav-ul-1-li nav-ul-1-li-sp">
+            <a href="{{ route('login') }}" class="nav-ul-1-li-a"><span class="nav-ul-1-li-a-span nav-ul-1-li-a-jimukyoku">事務局ログイン</span></a>
+          </li>
+          <li class="nav-ul-1-li nav-ul-1-li-sp">
+            <a href="{{ route('links') }}" class="nav-ul-1-li-a"><span class="nav-ul-1-li-a-span nav-ul-1-li-a-links">リンク</span></a>
+          </li>
+          <li class="nav-ul-1-li nav-ul-1-li-sp">
+            <a href="{{ route('privacy') }}" class="nav-ul-1-li-a"><span class="nav-ul-1-li-a-span nav-ul-1-li-a-privacy">個人情報保護方針</span></a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  </header>
+
+  <div id="main" class="contents">
+    <div class="second-mainvis-box privacy-mainvis">
+      <h1 class="second-mainvis-pagetitle"><span class="privacy-pagetitle">個人情報保護方針</span></h1>
+    </div>
+
+    <section class="contents-section">
+      <div class="contents-section-inner fadeInUp">
+        <article class="privacy-box">
+          <div class="privacy-preface">
+            株式会社 xxxx（以下「当社」といいます）は、以下のとおり個人情報保護方針を定め、個人情報保護の仕組みを構築し、全従業員に個人情報保護の重要性の認識と取組みを徹底させることにより、個人情報の保護を推進致します。
+          </div>
+          <dl class="privacy-dl">
+            <dt class="privacy-dt">個人情報の管理</dt>
+            <dd class="privacy-dd">当社は、お客さまの個人情報を正確かつ最新の状態に保ち、個人情報への不正アクセス・紛失・破損・改ざん・漏洩などを防止するため、セキュリティシステムの維持・管理体制の整備・社員教育の徹底等の必要な措置を講じ、安全対策を実施し個人情報の厳重な管理を行ないます。</dd>
+            <dt class="privacy-dt">個人情報の利用目的</dt>
+            <dd class="privacy-dd">お客さまからお預かりした個人情報は、当社からのご連絡や業務のご案内やご質問に対する回答として、電子メールや資料のご送付に利用いたします。</dd>
+            <dt class="privacy-dt">個人情報の第三者への開示・提供の禁止</dt>
+            <dd class="privacy-dd">当社は、お客さまよりお預かりした個人情報を適切に管理し、次のいずれかに該当する場合を除き、個人情報を第三者に開示いたしません。
+              <ul class="list-dot-ul">
+                <li class="list-dot-ul-li">お客さまの同意がある場合</li>
+                <li class="list-dot-ul-li">お客さまが希望されるサービスを行なうために当社が業務を委託する業者に対して開示する場合</li>
+                <li class="list-dot-ul-li">法令に基づき開示することが必要である場合</li>
+              </ul>
+            </dd>
+            <dt class="privacy-dt">個人情報の安全対策</dt>
+            <dd class="privacy-dd">当社は、個人情報の正確性及び安全性確保のために、セキュリティに万全の対策を講じています。</dd>
+            <dt class="privacy-dt">ご本人の照会</dt>
+            <dd class="privacy-dd">お客さまがご本人の個人情報の照会・修正・削除などをご希望される場合には、ご本人であることを確認の上、対応させていただきます。</dd>
+            <dt class="privacy-dt">法令、規範の遵守と見直し</dt>
+            <dd class="privacy-dd">当社は、保有する個人情報に関して適用される日本の法令、その他規範を遵守するとともに、本ポリシーの内容を適宜見直し、その改善に努めます。</dd>
+            <dt class="privacy-dt">お問い合せ</dt>
+            <dd class="privacy-dd">当社の個人情報の取扱に関するお問い合せは下記までご連絡ください。<br />
+              <div class="privacy-contact-box">
+                <div class="privacy-contact-box-name">公益社団法人 <span class="ilb">兵庫県保育協会</span></div>
+                〒651-0062<br />兵庫県神戸市中央区坂口通2丁目1番1号 <span class="ilb">兵庫県福祉センター内</span><br />
+                TEL：078-242-4623<br />
+                FAX：078-242-1399
+              </div>
+            </dd>
+          </dl>
+
+        </article>
+
+      </div>
+    </section>
+
+  </div><!-- main -->
+
+  <!-- footer -->
+  <footer class="footer">
+    <div class="footer-box-1">
+      <div class="contents-section-inner">
+        <div class="footer-logo">
+          <div class="footer-logo-txt">公益社団法人 兵庫県保育協会</div>
+          <address class="footer-logo-address">〒651-0062<br />兵庫県神戸市中央区坂口通2丁目1番1号 <span class="ilb">兵庫県福祉センター内</span></address>
+          <div class="footer-logo-tel">TEL：078-242-4623<br class="footer-logo-tel-br" /><span class="footer-logo-tel-separator">&nbsp;/&nbsp;</span>FAX：078-242-1399</div>
+        </div>
+      </div>
+    </div>
+    <div class="footer-box-2">
+      <div class="contents-section-inner">
+        <div class="footer-copyright">
+          &copy;2020 公益社団法人 兵庫県保育協会
+        </div>
+      </div>
+    </div>
+  </footer>
+
+</div><!--#wrp-->
+<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"></script>
+<script src="{{ asset('js/web/swiper.js') }}"></script>
+<script src="{{ asset('js/web/common.js') }}"></script>
+<script>
+$(function(){
+  indexInitialSetting();
+});
+</script>
+</body>
+</html>
