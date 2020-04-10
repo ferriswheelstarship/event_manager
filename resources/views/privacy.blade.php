9aafba216e367794f1d23c445893a762bdd0db5e
Itotagawa <itotagawa@mj-star.jp>
2020-04-10 18:59:48 +0900

    ページ更新

--- resources/views/privacy.blade.php
+++ resources/views/privacy.blade.php
@@ -107,38 +107,186 @@ Internet Explorerの安全ではないバージョンをお使いのようです
     <section class="contents-section">
       <div class="contents-section-inner fadeInUp">
         <article class="privacy-box">
-          <div class="privacy-preface">
-            株式会社 xxxx（以下「当社」といいます）は、以下のとおり個人情報保護方針を定め、個人情報保護の仕組みを構築し、全従業員に個人情報保護の重要性の認識と取組みを徹底させることにより、個人情報の保護を推進致します。
-          </div>
           <dl class="privacy-dl">
-            <dt class="privacy-dt">個人情報の管理</dt>
-            <dd class="privacy-dd">当社は、お客さまの個人情報を正確かつ最新の状態に保ち、個人情報への不正アクセス・紛失・破損・改ざん・漏洩などを防止するため、セキュリティシステムの維持・管理体制の整備・社員教育の徹底等の必要な措置を講じ、安全対策を実施し個人情報の厳重な管理を行ないます。</dd>
-            <dt class="privacy-dt">個人情報の利用目的</dt>
-            <dd class="privacy-dd">お客さまからお預かりした個人情報は、当社からのご連絡や業務のご案内やご質問に対する回答として、電子メールや資料のご送付に利用いたします。</dd>
-            <dt class="privacy-dt">個人情報の第三者への開示・提供の禁止</dt>
-            <dd class="privacy-dd">当社は、お客さまよりお預かりした個人情報を適切に管理し、次のいずれかに該当する場合を除き、個人情報を第三者に開示いたしません。
-              <ul class="list-dot-ul">
-                <li class="list-dot-ul-li">お客さまの同意がある場合</li>
-                <li class="list-dot-ul-li">お客さまが希望されるサービスを行なうために当社が業務を委託する業者に対して開示する場合</li>
-                <li class="list-dot-ul-li">法令に基づき開示することが必要である場合</li>
-              </ul>
+            <dt class="privacy-dt">（目的）</dt>
+            <dd class="privacy-dd">
+              <div class="privacy-dd-child">第１条　この規程は、個人情報が個人の人格尊重の理念のもとに慎重に取り扱われるべきものであることにかんがみ、公益社団法人兵庫県保育協会（以下「協会」という。）が保有する個人情報の適正な取扱いの確保に関し必要な事項を定めることにより、協会の事業の適正かつ円滑な運営を図りつつ、個人の権利利益を保護することを目的とする。</div>
             </dd>
-            <dt class="privacy-dt">個人情報の安全対策</dt>
-            <dd class="privacy-dd">当社は、個人情報の正確性及び安全性確保のために、セキュリティに万全の対策を講じています。</dd>
-            <dt class="privacy-dt">ご本人の照会</dt>
-            <dd class="privacy-dd">お客さまがご本人の個人情報の照会・修正・削除などをご希望される場合には、ご本人であることを確認の上、対応させていただきます。</dd>
-            <dt class="privacy-dt">法令、規範の遵守と見直し</dt>
-            <dd class="privacy-dd">当社は、保有する個人情報に関して適用される日本の法令、その他規範を遵守するとともに、本ポリシーの内容を適宜見直し、その改善に努めます。</dd>
-            <dt class="privacy-dt">お問い合せ</dt>
-            <dd class="privacy-dd">当社の個人情報の取扱に関するお問い合せは下記までご連絡ください。<br />
-              <div class="privacy-contact-box">
-                <div class="privacy-contact-box-name">公益社団法人 <span class="ilb">兵庫県保育協会</span></div>
-                〒651-0062<br />兵庫県神戸市中央区坂口通2丁目1番1号 <span class="ilb">兵庫県福祉センター内</span><br />
-                TEL：078-242-4623<br />
-                FAX：078-242-1399
+
+            <dt class="privacy-dt">（定義）</dt>
+            <dd class="privacy-dd">
+              <div class="privacy-dd-child">第２条　この規程における用語の定義は、次の各号に定めるところによる。
+                <ol class="list-number-ol">
+                  <li class="list-number-ol-li">個人情報  生存する個人に関する情報であって、当該情報に含まれる氏名、生年月日その他の記述又は個人別に付された番号、記号その他の符号により当該個人を識別できるもの（当該情報のみでは識別できないが、他の情報と容易に照合することができ、それにより当該個人を識別できることとなるものを含む。）をいう。</li>
+                  <li class="list-number-ol-li">個人情報データベース等　特定の個人情報をコンピュータを用いて検索することができるように体系的に構成した個人情報を含む情報の集合物、又はコンピュータを用いていない場合であっても、紙媒体で処理した個人情報を一定の規則にしたがって整理又は分類し、特定の個人情報を容易に検索することができる状態においているものをいう。</li>
+                  <li class="list-number-ol-li">個人データ　個人情報データベース等を構成する個人情報をいう。</li>
+                  <li class="list-number-ol-li">保有個人データ　協会が開示、訂正、追加、削除、利用の停止、消去及び第三者への提供の停止を行うことのできる権限を有する個人データであって、その存否が明らかになることにより、本人又は第三者の生命、身体又は財産に危害が及ぶおそれがあるもの、又は違法若しくは不当な行為を助長し、又は誘発するおそれがあるもの以外をいう。</li>
+                  <li class="list-number-ol-li">本人　個人情報から識別され、又は識別され得る個人をいう。</li>
+                  <li class="list-number-ol-li">従業者　協会にあって直接間接に協会の指揮監督を受けて、協会の業務に従事している者等をいい、雇用関係にある従業員（正社員、契約社員、嘱託社員、パート社員、アルバイト社員等）のみならず、理事、監事も含まれる。</li>
+                </ol>
               </div>
             </dd>
+
+            <dt class="privacy-dt">（協会の責務）</dt>
+            <dd class="privacy-dd">
+              <div class="privacy-dd-child">第３条　協会は、個人情報保護に関する法令等を遵守するとともに、実施するあらゆる事業を通じて個人情報の保護に努めるものとする。</div>
+            </dd>
+
+            <dt class="privacy-dt">（利用目的の特定、変更）</dt>
+            <dd class="privacy-dd">
+              <div class="privacy-dd-child">第４条　協会は、個人情報を取り扱うに当たっては、その利用の目的（以下「利用目的」という。）をできる限り特定するものとする。</div>
+              <div class="privacy-dd-child">２　協会は、利用目的を変更する場合には、変更前の利用目的と相当の関連性を有すると合理的に認められる範囲で行うものとする。</div>
+              <div class="privacy-dd-child">３　協会は、利用目的を変更した場合は、変更した利用目的について、本人に通知し、又は公表するものとする。</div>
+            </dd>
+            <dt class="privacy-dt">（個人情報の利用目的）</dt>
+            <dd class="privacy-dd">
+              <div class="privacy-dd-child">第５条　協会が取り扱う個人情報の利用目的は、次のとおりとする。
+                <ol class="list-ol">
+                  <li class="list-ol-li">入会時の確認、承認に関する会員台帳等会員情報管理</li>
+                  <li class="list-ol-li">会費等の請求・収納及び自動引落に関する徴収管理</li>
+                  <li class="list-ol-li">総会等の主要な会議の案内及び会議運営・議決権の行使に係る事務管理</li>
+                  <li class="list-ol-li">協会が発行する機関誌等の編纂・送付に関する事務管理</li>
+                  <li class="list-ol-li">協会の活動についての広報・宣伝等の広報活動</li>
+                  <li class="list-ol-li">研修会、大会等イベントの参加者情報等イベント業務の管理</li>
+                  <li class="list-ol-li">電子メール、電話、郵送等各種媒体により、協会が行うアンケート調査</li>
+                  <li class="list-ol-li">行政及び関係団体からの委員等の派遣要請に基づく連絡、調整</li>
+                  <li class="list-ol-li">保育人材確保対策貸付事業の申請に関する貸与者情報管理</li>
+                  <li class="list-ol-li">その他協会の事業活動のため、組織運営上必要な業務事業者の管理及び雇用契約に基づく事項の履行</li>
+                </ol>
+              </div>
+            </dd>
+
+            <dt class="privacy-dt">（利用目的外の利用の制限）</dt>
+            <dd class="privacy-dd">
+              <div class="privacy-dd-child">第６条　協会は、あらかじめ本人の同意を得ることなく前２条の規定により特定された利用目的の達成に必要な範囲を超えて個人情報を取り扱わないものとする。</div>
+              <div class="privacy-dd-child">２　協会は、合併その他の事由により他の法人等から事業を継承することに伴って個人情報を取得した場合は、あらかじめ本人の同意を得ないで継承前における当該個人情報の利用目的の達成に必要な範囲を超えて、当該個人情報を取り扱わないものとする。</div>
+              <div class="privacy-dd-child">３　前２項の規定にかかわらず、次の各号のいずれかに該当する場合には、あらかじめ本人の同意を得ないで前２条の規定により特定された利用目的の範囲を超えて個人情報を取り扱うことができるものとする。
+                <ol class="list-number-ol">
+                  <li class="list-number-ol-li">法令に基づく場合</li>
+                  <li class="list-number-ol-li">人の生命、身体又は財産の保護のために必要がある場合であって、本人の同意を得ることが困難であるとき。</li>
+                  <li class="list-number-ol-li">公衆衛生の向上又は児童の健全な育成の推進のために特に必要がある場合であって、本人の同意を得ることが困難であるとき。</li>
+                  <li class="list-number-ol-li">国の機関若しくは地方公共団体又はその委託を受けた者が法令の定める事務を遂行することに対して協力する必要がある場合であって、本人の同意を得ることにより、当該事務の遂行に支障を及ぼすおそれがあるとき。</li>
+                </ol>
+              </div>
+              <div class="privacy-dd-child">４　協会は、前項の規定に該当して利用目的の範囲を超えて個人情報を取り扱う場合には、その取扱う範囲を真に必要な範囲に限定するものとする。</div>
+            </dd>
+
+            <dt class="privacy-dt">（取得の制限）</dt>
+            <dd class="privacy-dd">
+              <div class="privacy-dd-child">第７条　協会は、個人情報を取得するときは、利用目的を明示するとともに、適法かつ適正な方法で行うものとする。</div>
+              <div class="privacy-dd-child">２　協会は、思想、信条及び宗教に関する個人情報並びに社会的差別の原因となる個人情報については取得しないものとする。</div>
+              <div class="privacy-dd-child">３　協会は、原則として本人から個人情報を取得するものとする。ただし、次の各号のいずれかに該当する場合は、この限りでない。
+                <ol class="list-number-ol">
+                  <li class="list-number-ol-li">本人の同意があるとき。</li>
+                  <li class="list-number-ol-li">法令等の規定に基づくとき。</li>
+                  <li class="list-number-ol-li">個人の生命、身体又は財産の安全を守るため緊急かつやむを得ないと認められるとき。</li>
+                  <li class="list-number-ol-li">所在不明、判断能力が不十分等の事由により、本人から取得することができないとき。</li>
+                  <li class="list-number-ol-li">相談、援助、指導、代理、代行等を含む事業において、本人から取得したのではその目的を達成し得ないと認められるとき。</li>
+                </ol>
+              </div>
+              <div class="privacy-dd-child">４　協会は、前項第４号又は第５号の規定に該当して本人以外の者から個人情報を取得したときは、その旨及び当該個人情報に係る利用目的を本人に通知するよう努めるものとする。</div>
+            </dd>
+
+            <dt class="privacy-dt">（取得に際しての利用目的の通知等）</dt>
+            <dd class="privacy-dd">
+              <div class="privacy-dd-child">第８条　協会は、個人情報を取得した場合は、あらかじめその利用目的を公表している場合を除き、速やかに、その利用目的を本人に通知し、又は公表するものとする。</div>
+              <div class="privacy-dd-child">２　協会は、前項の規定にかかわらず、本人との間で契約を締結することに伴って契約書その他の書面に記載された当該本人の個人情報を取得する場合その他本人から直接書面に記載された当該本人の個人情報を取得する場合は、あらかじめ、本人に対し、その利用目的を明示するものとする。ただし、人の生命、身体又は財産の保護のために緊急に必要がある場合には、この限りでない。</div>
+              <div class="privacy-dd-child">３　前２項の規定は、次に掲げる場合については適用しない。
+                <ol class="list-number-ol">
+                  <li class="list-number-ol-li">利用目的を本人に通知し、又は公表することにより本人又は第三者の生命、身体、財産その他の権利利益を害するおそれがある場合</li>
+                  <li class="list-number-ol-li">国の機関若しくは地方公共団体又はその委託を受けた者が法令の定める事務を遂行することに対して協力する必要がある場合であって、利用目的を本人に通知し、又は公表することにより当該事務の遂行に支障を及ぼすおそれがあるとき</li>
+                </ol>
+              </div>
+            </dd>
+
+            <dt class="privacy-dt">（個人データの適正管理）</dt>
+            <dd class="privacy-dd">
+              <div class="privacy-dd-child">第９条　協会は、利用目的の達成に必要な範囲内で、常に個人データを正確かつ最新の状態に保つものとする。</div>
+              <div class="privacy-dd-child">２　協会は、個人データの漏えい、滅失、き損の防止その他の個人データの安全管理のために必要かつ適切な措置を講ずるものとする。</div>
+              <div class="privacy-dd-child">３　協会は、個人データの安全管理のために、個人データを取り扱う従業者に対する必要かつ適切な監督を行うものとする。</div>
+              <div class="privacy-dd-child">４　協会は、利用目的に関し保存する必要がなくなった個人データを、確実、かつ速やかに破棄又は削除するものとする。</div>
+              <div class="privacy-dd-child">５　協会は、個人情報の取扱いの全部又は一部を協会以外の者に委託するときは、原則として委託契約において、個人データの安全管理について受託者が講ずべき措置を明らかにし、受託者に対する必要かつ適切な監督を行うものとする。</div>
+            </dd>
+
+            <dt class="privacy-dt">（個人データの第三者提供）</dt>
+            <dd class="privacy-dd">
+              <div class="privacy-dd-child">第１０条　協会は、次に掲げる場合を除くほか、あらかじめ本人の同意を得ないで、個人データを第三者に提供しないものとする。
+                <ol class="list-number-ol">
+                  <li class="list-number-ol-li">法令に基づく場合</li>
+                  <li class="list-number-ol-li">人の生命、身体又は財産の保護のために必要がある場合であって、本人の同意を得ることが困難であるとき</li>
+                  <li class="list-number-ol-li">公衆衛生の向上又は児童の健全な育成の推進のために特に必要がある場合であって、本人の同意を得ることが困難であるとき</li>
+                  <li class="list-number-ol-li">国の機関若しくは地方公共団体又はその委託を受けた者が法令の定める事務を遂行することに対して協力する必要がある場合であって、本人の同意を得ることにより、当該事務の遂行に支障を及ぼすおそれがあるとき</li>
+                </ol>
+              </div>
+              <div class="privacy-dd-child">２　次に掲げる場合において、当該個人データの提供を受ける者は、前項の規定の適用については、第三者に該当しないものとする。
+                <ol class="list-number-ol">
+                  <li class="list-number-ol-li">協会が利用目的の達成に必要な範囲内において個人データの取扱いの全部又は一部を委託する場合</li>
+                  <li class="list-number-ol-li">合併その他の事由による事業の承継に伴って個人データが提供される場合</li>
+                  <li class="list-number-ol-li">個人データを特定の者との間で共同して利用する場合であって、その旨並びに共同して利用される個人データの項目、共同して利用する者の範囲、利用する者の利用目的及び当該個人データの管理について責任を有する者の氏名又は名称についてあらかじめ本人に通知し、又は本人が容易に知り得る状態に置いているとき</li>
+                </ol>
+              </div>
+              <div class="privacy-dd-child">３　協会は、前項第３号に規定する利用する者の利用目的又は個人データの管理について責任を有する者の氏名又は名称を変更する場合は、変更する内容について、あらかじめ本人に通知し、又は本人が容易に知り得る状態に置くものとする。</div>
+            </dd>
+
+            <dt class="privacy-dt">（保有個人データの開示等）</dt>
+            <dd class="privacy-dd">
+              <div class="privacy-dd-child">
+                第１１条　協会は、本人から、当該本人に係る保有個人データについて、書面又は口頭により、その開示（当該本人が識別される個人情報を保有していないときにその旨を知らせることを含む。以下同じ。）の申し出があったときは、身分証明書等により本人であることを確認の上、開示をするものとする。ただし、開示することにより次の各号のいずれかに該当する場合は、その全部又は一部を開示しないことができる。
+                <ol class="list-number-ol">
+                  <li class="list-number-ol-li">本人又は第三者の生命、身体、財産その他の権利利益を害するおそれがある場合</li>
+                  <li class="list-number-ol-li">協会の事業の適正な実施に著しい支障を及ぼすおそれがある場合</li>
+                  <li class="list-number-ol-li">他の法令に違反することとなる場合</li>
+                </ol>
+              </div>
+              <div class="privacy-dd-child">２　開示は、書面により行うものとする。ただし、開示の申出をした者の同意があるときは、書面以外の方法により開示をすることができる。</div>
+              <div class="privacy-dd-child">３　保有個人データの開示又は不開示の決定の通知は、本人に対し書面により遅滞なく行うものとする。</div>
+            </dd>
+
+            <dt class="privacy-dt">（保有個人データの訂正、追加、削除、利用停止、等）</dt>
+            <dd class="privacy-dd">
+              <div class="privacy-dd-child">第１２条　協会は、保有個人データの開示を受けた者から、書面又は口頭により、開示に係る個人データの訂正、追加、削除又は利用停止の申出があったときは、利用目的の達成に必要な範囲内において遅滞なく調査を行い、その結果を申出をした者に対し、書面により通知するものとする。</div>
+              <div class="privacy-dd-child">２　協会は、前項の通知を受けた者から、再度申出があったときは、前項と同様の処理を行うものとする。</div>
+            </dd>
+
+            <dt class="privacy-dt">（個人情報保護管理者）</dt>
+            <dd class="privacy-dd">
+              <div class="privacy-dd-child">第１３条　協会は、個人情報の適正管理のため個人情報保護管理者を定め、協会における個人情報の適正管理に必要な措置を行わせるものとする。</div>
+              <div class="privacy-dd-child">２　個人情報保護管理者は、事務局長とする。</div>
+              <div class="privacy-dd-child">３　事務局長は、会長の指示及び本規程の定めに基づき、適正管理対策の実施、従業者に対する教育・事業訓練等を行う責任を負うものとする。</div>
+              <div class="privacy-dd-child">４　事務局長は、適正管理に必要な措置について定期的に評価を行い、見直し又は改善を行うものとする。</div>
+              <div class="privacy-dd-child">５　事務局長は、個人情報の適正管理に必要な措置の一部を各事業を分掌する従業者に委任することができる。</div>
+            </dd>
+
+            <dt class="privacy-dt">（苦情対応）</dt>
+            <dd class="privacy-dd">
+              <div class="privacy-dd-child">第１４条　協会は、個人情報の取扱いに関する苦情（以下「苦情」という。）について必要な体制整備を行い、苦情があったときは、適切かつ迅速な対応に努めるものとする。</div>
+              <div class="privacy-dd-child">２　苦情対応の責任者は、事務局長とするものとする。</div>
+              <div class="privacy-dd-child">３　苦情の処理に対応した従業者は、適宜、個人情報保護管理者に苦情の内容について報告するものとする。</div>
+            </dd>
+
+            <dt class="privacy-dt">（従業者の義務）</dt>
+            <dd class="privacy-dd">
+              <div class="privacy-dd-child">第１５条　協会の従業者又は従業者であった者は、業務上知り得た個人情報の内容をみだりに他人に知らせ、又は不当な目的に使用してはならない。</div>
+              <div class="privacy-dd-child">２　本規程に違反する事実又は違反するおそれがあることを発見した従業者は、その旨を個人情報保護管理者に報告するものとする。</div>
+              <div class="privacy-dd-child">３　個人情報保護管理者は、前項による報告の内容を調査し、違反の事実が判明した場合には遅滞なく会長に報告するとともに、速やかに違反の事実の解消のため適切な措置を講じるものとする。</div>
+            </dd>
+
+            <dt class="privacy-dt">（改廃）</dt>
+            <dd class="privacy-dd"><div class="privacy-dd-child">第１６条　この規程の改廃は、理事会において行うものとする。</div></dd>
+
+
+            <dt class="privacy-dt">附　則</dt>
+            <dd class="privacy-dd">この規程は、令和元年8月27日から施行する。</dd>
           </dl>
+
+
+          <div class="privacy-contact-box">
+            お問い合わせ
+            <div class="privacy-contact-box-name">公益社団法人 <span class="ilb">兵庫県保育協会</span></div>
+            〒651-0062<br />兵庫県神戸市中央区坂口通2丁目1番1号 <span class="ilb">兵庫県福祉センター内</span><br />
+            TEL：078-242-4623<br />
+            FAX：078-242-1399
+          </div>
 
         </article>
 
