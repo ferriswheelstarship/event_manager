29f98e481971b95a0d38724310a4c8ef35c8dde9
Itotagawa <itotagawa@mj-star.jp>
2020-05-15 13:37:40 +0900

    修正

--- resources/views/account/regist.blade.php
+++ resources/views/account/regist.blade.php
@@ -56,7 +56,7 @@
                                 <div class="form-group row">
                                     <label for="email" class="col-md-4 col-form-label text-md-right">メールアドレス</label>
                                     <div class="col-md-6">
-                                        <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" >
+                                        <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required>
 
                                         @if ($errors->has('email'))
                                             <span class="invalid-feedback">
