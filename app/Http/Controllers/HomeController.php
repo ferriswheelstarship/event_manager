ccc42eecfd78ca4bfe5eb17fc38b80102d2e182d
Itotagawa <itotagawa@mj-star.jp>
2020-08-26 10:34:04 +0900

    検証

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -263,12 +263,9 @@ class HomeController extends Controller
                             } 
                         }
                     }
-                    
                     $data['event_ticket_sended'] = 
                         (count($data_event_ticket_sended) > 0) 
                             ? $this->getUniqueArray($data_event_ticket_sended,'event_date') : [];
-
-                    dd($data['event_ticket_sended']);
 
                     // 開催間近の研修（受講券未発行）
                     $data_event_ticket_none = [];
